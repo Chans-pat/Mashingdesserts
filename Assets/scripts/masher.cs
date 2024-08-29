7aca1cbc80da61f667868daf5fc19327401ccf63
Chans-pat <chanslarpatton@gmail.com>
2024-08-29 13:10:51 -0500

    m

--- Assets/scripts/masher.cs
+++ Assets/scripts/masher.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 
 public class masher : MonoBehaviour
 {
-    Animator ani;
-    // Start is called before the first frame update
+    
+    
     void Start()
     {
-        ani = GetComponent<Animator>();
+
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            
-        }
+
     }
 }
