14b49269e16fc8d9183e834b9171976cf128e510
Chans-pat <chanslarpatton@gmail.com>
2024-08-29 13:17:09 -0500

    d

--- Assets/scripts/mainmenu.cs
+++ Assets/scripts/mainmenu.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class mainmenu : MonoBehaviour
 {
-    
+    static string  masher;
     
     void Start()
     {
--- Assets/scripts/masher.cs
+++ Assets/scripts/masher.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class masher : MonoBehaviour
 {
-    
+    bool MasherA;
     
     void Start()
     {
